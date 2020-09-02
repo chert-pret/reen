436b1d911680cbdb905208583818bb94cab5d6fb
kek-blek <farewak2@gmail.com>
2020-09-02 23:07:58 +0300

    Added JQuery

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
-'use strict';
+// let megaMenu = document.querySelector('.mega-menu');
+// let menuButton = document.querySelector('.button-show');
 
-let megaMenu = document.querySelector('.mega-menu');
-let menuButton = document.querySelector('.button-show');
+// menuButton.onclick = function() {
+//     menuButton.classList.toggle('button-color');
+//     megaMenu.classList.toggle('show');
+//     megaMenu.classList.toggle('hide');
+//   };
 
-menuButton.onclick = function() {
-    menuButton.classList.toggle('button-color');
-    megaMenu.classList.toggle('show');
-    megaMenu.classList.toggle('hide');
-  };
+$(function() {
+
+  // Fixed Header
+  let headerH = $("#hero").innerHeight(),
+      fixedHeader = $("#fixed-header"),
+      scrollOffset = $(window).scrollTop(); // Scroll value in pixels
+  
+   checkScroll(scrollOffset)
+   
+    $(window).on("scroll", function() {
+      scrollOffset = $(this).scrollTop();
+
+      checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+        if( scrollOffset >= headerH ) {
+          fixedHeader.addClass("fixed");
+        } else {
+          fixedHeader.removeClass("fixed");
+        }
+    }
+
+    // Smooth scroll
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("html, body").animate({
+            scrollTop:  blockOffset
+        }, 500);
+    });
+
+    // Menu nav toggle
+    $("#nav-toggle").on("click", function(event) {
+      event.preventDefault();
+
+      $(this).toggleClass("active");
+      $("#nav").toggleClass("active");
+    })
+});
